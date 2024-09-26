0bc057f7370c18363f88b172e08f4003ef4c676d
croll5 <cecile.rolland@eleve.isep.fr>
2024-09-26 22:32:51 +0200

    Version particulièrement primaire d'affichage de l'arborescence

--- modules/arborescence/arborecsence.go
+++ modules/arborescence/arborecsence.go
@@ -1,12 +1,14 @@
 package arborescence
 
 import (
+	"encoding/csv"
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/bodgit/sevenzip"
 )
@@ -14,6 +16,62 @@ import (
 type Arborescence struct {
 	Nom     string         `json:"nom"`
 	Enfants []Arborescence `json:"enfants,omitempty"`
+}
+
+func remplitArborescenceDepuisCSV(fichierCSV *sevenzip.File, arbo *Arborescence) error {
+	log.Println("INFO | Traitement du fichier", fichierCSV.FileInfo().Name())
+	contenuBrute, err := fichierCSV.Open()
+	if err != nil {
+		return err
+	}
+	var contenuCSV *csv.Reader = csv.NewReader(contenuBrute)
+	for {
+		ligne, err := contenuCSV.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		var chemin []string = strings.Split(ligne[4], "\\")
+		var vousetesici *[]Arborescence = &arbo.Enfants
+
+		for _, dossier := range chemin {
+			var dossierExiste bool = false
+			for i := range *vousetesici {
+				if (*vousetesici)[i].Nom == dossier {
+					vousetesici = &(*vousetesici)[i].Enfants
+					dossierExiste = true
+					break
+				}
+			}
+			if !dossierExiste {
+				nouveauDossier := Arborescence{
+					Nom:     dossier,
+					Enfants: []Arborescence{},
+				}
+				*vousetesici = append(*vousetesici, nouveauDossier)
+				// Mettre à jour le pointeur pour le nouveau dossier
+				vousetesici = &(*vousetesici)[len(*vousetesici)-1].Enfants
+			}
+		}
+
+	}
+	return nil
+}
+
+func enregistrerArborescenceJson(arbo *Arborescence, chemin string) error {
+	donneesArbo, err := json.Marshal(arbo)
+	if err != nil {
+		return err
+	}
+	fichier, err := os.Create(chemin)
+	if err != nil {
+		return err
+	}
+	defer fichier.Close()
+	_, err = fichier.Write(donneesArbo)
+	return err
 }
 
 func GetArborescence(cheminProjet string) (Arborescence, error) {
@@ -29,6 +87,8 @@ func GetArborescence(cheminProjet string) (Arborescence, error) {
 
 func ExtraireArborescence(cheminProjet string) (Arborescence, error) {
 	var resultatArbo Arborescence = Arborescence{}
+	resultatArbo.Nom = "racine"
+	resultatArbo.Enfants = []Arborescence{}
 	// On parcourt les fichiers GetTHis
 	collectes, err := os.ReadDir(filepath.Join(filepath.Dir(cheminProjet), "collecteORC"))
 	if err != nil {
@@ -47,14 +107,15 @@ func ExtraireArborescence(cheminProjet string) (Arborescence, error) {
 				if err != nil {
 					return err
 				}
-				for _, f := range r.File {
-					if f.Name == "GetThis.csv" {
-						fmt.Println(f.FileInfo().Name())
+				for _, fichierCSV := range r.File {
+					if fichierCSV.Name == "GetThis.csv" {
+						remplitArborescenceDepuisCSV(fichierCSV, &resultatArbo)
 					}
 				}
 				return nil
 			})
 		}
 	}
-	return resultatArbo, nil
+	err = enregistrerArborescenceJson(&resultatArbo, filepath.Join(filepath.Dir(cheminProjet), "analyse", "arborescence.json"))
+	return resultatArbo, err
 }
