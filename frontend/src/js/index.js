3f1d36a220aa9fbc66a443610e4b1dbde4272e90
croll5 <cecile.rolland@eleve.isep.fr>
2024-12-05 08:12:34 +0100

    Refonte de l'affichage (modification de l'emplacement des fichiers)

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,21 +1,17 @@
-/** Fonction appelée lorsque l'utilisateur appuie sur le bouton "nouvelle analyse"
- */
-function nouvelle_analyse(){
-    window.location.replace("../html/nouvelle_analyse.html");
+function change_onglet(destination, id_onglet){
+    document.getElementsByTagName("iframe")[0].src = destination;
+    // On met tous les autres onglets à la couleur standard
+    let onglets = document.getElementsByClassName("onglet");
+    for (const onglet of onglets) {
+        onglet.style.backgroundColor = "#856B0D";
+        onglet.style.color = "#fff";
+    }
+    // On met l'onglet sur lequel on va à la couleur de la page
+    let onglet_courant = parent.document.getElementById(id_onglet);
+    onglet_courant.style.backgroundColor = "#FCF5DC";
+    onglet_courant.style.color = "#000";
 }
 
-function analyse_existante(){
-    parent.window.go.main.App.OuvrirAnalyseExistante().then(resultat=>{
-        if(resultat){
-            window.location.replace("../html/extraction.html")
-            //parent.document.getElementsByTagName("header")[0].style.display = "inline";
-            //let onglet_courant = parent.document.getElementById("onglet_extraction");
-            //onglet_courant.style.backgroundColor = "#FCF5DC";
-            //onglet_courant.style.color = "#000";
-        }
-    })
-}
-
-function nouveau_modele(){
-    window.location.replace("../html/nouveau_modele.html");
+function accueil(){
+    window.location.replace("/")
 }
